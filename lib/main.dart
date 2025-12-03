f2296a36baf4b1d523c2d2377a48cb86d5af3831
arcanefairy <130689169+arcanefairy@users.noreply.github.com>
2025-12-03 01:30:08 -0700

    Actualización del botón de iniciar juego (Se le pide al usuario un username para registrar su puntaje en la aplicación, al finalizar su partida, muestra su puntaje actual y si existe un mejor puntaje registrado, lo muestra como mejor récord)

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'menu_page.dart';
 import 'global_background.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
+
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+  );
 
   runApp(const MyApp());
 }
